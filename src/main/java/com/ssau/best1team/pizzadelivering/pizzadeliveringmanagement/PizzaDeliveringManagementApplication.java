bc34c1c6d0a33109a36d523bdc7b3ce656143e4a
VladimirBai <bajjramov.vladimir@gmail.com>
2020-10-12 12:55:44 +0400

    project carcass

---
+++ src/main/java/com/ssau/best1team/pizzadelivering/pizzadeliveringmanagement/PizzaDeliveringManagementApplication.java
@@ -0,0 +1,13 @@
+package com.ssau.best1team.pizzadelivering.pizzadeliveringmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaDeliveringManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PizzaDeliveringManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ssau/best1team/pizzadelivering/pizzadeliveringmanagement/PizzaDeliveringManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssau.best1team.pizzadelivering.pizzadeliveringmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaDeliveringManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
