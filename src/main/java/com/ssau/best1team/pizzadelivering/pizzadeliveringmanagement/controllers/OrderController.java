1924e59e266dcbc34566058a0fe0e7b55f11282f
VladimirBai <bajjramov.vladimir@gmail.com>
2020-12-19 04:38:22 +0400

    allow courier modify order status

--- src/main/java/com/ssau/best1team/pizzadelivering/pizzadeliveringmanagement/controllers/OrderController.java
+++ src/main/java/com/ssau/best1team/pizzadelivering/pizzadeliveringmanagement/controllers/OrderController.java
@@ -5,6 +5,7 @@ import com.ssau.best1team.pizzadelivering.pizzadeliveringmanagement.dto.OrderSta
 import com.ssau.best1team.pizzadelivering.pizzadeliveringmanagement.exceptions.EntityNotFoundException;
 import com.ssau.best1team.pizzadelivering.pizzadeliveringmanagement.services.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -25,7 +26,8 @@ public class OrderController {
         return orderService.findAll();
     }
 
-    @PutMapping(value = "admin/orders/{orderId}/status/{statusId}")
+    @PreAuthorize("hasAuthority('ADMIN') and hasAuthority('COURIER')")
+    @PutMapping(value = "/orders/{orderId}/status/{statusId}")
     public OrderDTO updateOrderStatus(@PathVariable long orderId, @PathVariable long statusId) throws EntityNotFoundException {
         return orderService.modifyOrderStatus(orderId, statusId);
     }
