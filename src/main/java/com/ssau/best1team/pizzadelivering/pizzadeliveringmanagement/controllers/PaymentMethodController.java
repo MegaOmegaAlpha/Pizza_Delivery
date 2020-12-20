e76aa5391b6305eb1eb78956e041505fdc2d041a
VladimirBai <bajjramov.vladimir@gmail.com>
2020-12-20 23:12:23 +0400

    fill classes

--- src/main/java/com/ssau/best1team/pizzadelivering/pizzadeliveringmanagement/controllers/PaymentMethodController.java
+++ src/main/java/com/ssau/best1team/pizzadelivering/pizzadeliveringmanagement/controllers/PaymentMethodController.java
@@ -1,2 +1,24 @@
-package com.ssau.best1team.pizzadelivering.pizzadeliveringmanagement.controllers;public class PaymentMethodController {
+package com.ssau.best1team.pizzadelivering.pizzadeliveringmanagement.controllers;
+
+import com.ssau.best1team.pizzadelivering.pizzadeliveringmanagement.dto.PaymentMethodDTO;
+import com.ssau.best1team.pizzadelivering.pizzadeliveringmanagement.services.PaymentMethodService;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:4200")
+public class PaymentMethodController {
+
+    private PaymentMethodService paymentMethodService;
+
+    @GetMapping(value = "/customer/payment-methods")
+    public List<PaymentMethodDTO> findAll() {
+        return paymentMethodService.findAll();
+    }
+
 }
